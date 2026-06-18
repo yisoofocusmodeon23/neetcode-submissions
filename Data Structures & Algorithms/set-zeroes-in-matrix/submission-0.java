3d523d858c304d683e065bb58a30848779c678c7
yisoofocusmodeon23 <yisoofocusmodeon@gmail.com>
2026-06-18 12:01:42 +0530

    Add: set-zeroes-in-matrix - submission-0

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        boolean firstColZero = false;
+        // updating markers
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+            }
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0; // col marker
+                    matrix[0][j] = 0; // row marker
+                }
+            }
+        }
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+            if (firstColZero) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
